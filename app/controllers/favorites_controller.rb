de8b883e1701e3b810304a823150c4071c312108
Daisuke-Sato-0530 <daisuke19940530@gmail.com>
2021-03-06 16:19:08 +0000

    いいねの非同期通信化実装

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,17 +1,17 @@
 class FavoritesController < ApplicationController
 
   def create
-    item = Item.find(params[:item_id])
-    favorite = current_user.favorites.new(item_id: item.id)
+    @item = Item.find(params[:item_id])
+    favorite = @item.favorites.new(user_id: current_user.id)
     favorite.save
-    redirect_to ranking_path(item.ranking)
+    #redirect_to ranking_path(@item.ranking) #非同期通信
   end
 
   def destroy
-    item = Item.find(params[:item_id])
-    favorite = current_user.favorites.find_by(item_id: item.id)
+    @item = Item.find(params[:item_id])
+    favorite = @item.favorites.find_by(user_id: current_user.id)
     favorite.destroy
-    redirect_to ranking_path(item.ranking)
+    #redirect_to ranking_path(@item.ranking) #非同期通信
   end
 
 end
