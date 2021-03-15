d12cf42bb4d02fad20315a4faf9763644d33e7df
Daisuke-Sato-0530 <daisuke19940530@gmail.com>
2021-03-15 09:57:15 +0000

    fontawesome入れました。

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
 
   root :to => 'homes#top'
   
+  devise_scope :user do #ゲストログイン用
+    post 'users/guest_sign_in', to: 'users/sessions#new_guest'
+  end
+  
   devise_for :users, controllers: {
     omniauth_callbacks: 'users/omniauth_callbacks'
   }
@@ -12,6 +16,8 @@ Rails.application.routes.draw do
     resources :comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
   end
+  
+  
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
