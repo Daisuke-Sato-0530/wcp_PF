48002dcde6a4a0f9d8f4e3671d53d063d4e9866a
Daisuke-Sato-0530 <daisuke19940530@gmail.com>
2021-03-19 09:38:58 +0000

    userコントローラー追加

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+
+  def show
+    @user = current_user
+    #@rankings = Item.joins(:favorites).group(:ranking_id)
+    @your_favorites = current_user.favorites
+  end
+
+  def edit
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+    @user.update(user_params)
+    redirect_to user_path(current_user)
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name,:email)
+    end
+
+end
