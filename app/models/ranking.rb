c4fb01626531cb608887d0a95b3c0466157e7c9f
Daisuke-Sato-0530 <daisuke19940530@gmail.com>
2021-03-31 03:14:02 +0000

    model直した

--- app/models/ranking.rb
+++ app/models/ranking.rb
@@ -1,9 +1,9 @@
 class Ranking < ApplicationRecord
   acts_as_taggable #tagのgemつかえるようにするやつ
   belongs_to :user
-  has_many :items,dependent: :delete_all
+  has_many :items,dependent: :destroy
   accepts_nested_attributes_for :items, allow_destroy: true #RankingとItemsを同時に保存する
-  
+
   validates :title, presence: true
   validates :title, length: { in: 2..30 }     # 2文字以上30文字以下
   validates :introduction, length: { maximum: 500 }     # 500文字以下
